61d87146a798a61ea3e00179f135bd7c65c1ec20
Murilo Ferraz Franco <murilo@inf.ufg.br>
2017-02-03 16:20:32 -0200

    Incluído menu lateral esquerdo Iniciada implementação da parte dos blocos de notícia.

--- inf-ufg/index.php
+++ inf-ufg/index.php
@@ -2,16 +2,70 @@
 
 <section id="principal" class="clear ">
     <div id="conteudo" class="total-centro">
-        <h1>Área principal</h1>
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <article class="post-article">
-            <header class="post-header"><?php the_title(); ?></header>
-            <h4>Postado em <?php the_time('F jS, Y') ?></h4>
-            <?php the_content(__('(more...)')); ?>
-        </article>
-        <hr> <?php endwhile; else: ?>
-        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
-        
+        <nav id="menu-lateral" class="clear">
+            <ul class="menu-lateral-topo">
+                <li class="menu-lateral-titulo"><a href="#">Graduação</a></li>
+                <ul class="menu-lateral-sub">
+                    <li class="menu-lateral-subitem"><a href="#">Ciências da Computação</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">Engenharia de Software</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">Sistemas de Informação</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">Estágio</a></li>
+                </ul>
+                <li class="menu-lateral-titulo"><a href="#">Pós-Graduação</a></li>
+                <ul class="menu-lateral-sub">
+                    <li class="menu-lateral-subitem"><a href="#">Mestrado</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">Doutorado</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">Especializações</a></li>
+                </ul>
+                <li class="menu-lateral-titulo"><a href="#">Pesquisa</a></li>
+                <ul class="menu-lateral-sub">
+                    <li class="menu-lateral-subitem"><a href="#">Objetivos</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">Grupos de Pesquisa</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">Projetos</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">Compêndio</a></li>
+                </ul>
+                <li class="menu-lateral-titulo"><a href="#">Extensão</a></li>
+                <ul class="menu-lateral-sub">
+                    <li class="menu-lateral-subitem"><a href="#">Objetivo</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">APOEMA</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">CFTI</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">Maratona de Programação</a></li>
+                </ul>
+                <li class="menu-lateral-titulo"><a href="#">UFG</a></li>
+                <ul class="menu-lateral-sub">
+                    <li class="menu-lateral-subitem"><a href="#">Biblioteca Central</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">Portal da UFG</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">Portal SIG</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">Portal UFGNet</a></li>
+                </ul>
+                <li class="menu-lateral-titulo"><a href="#">Portais externos</a></li>
+                <ul class="menu-lateral-sub">
+                    <li class="menu-lateral-subitem"><a href="#">CAPES</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">CNPQ</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">FINEP</a></li>
+                    <li class="menu-lateral-subitem"><a href="#">FUNAPE</a></li>
+                </ul>
+            </ul>
+        </nav>
+    
+        <div id="area-conteudo" class="">
+            <section class="bloco-conteudo">
+                <header class="cabecalho-bloco"><div class="bloco-titulo">Um bloco de conteúdo</div><div class="bloco-ver-todas"><a href="#">Ver todas</a></div></header>
+                <div class="caixa-conteudo-texto caixa-conteudo">
+                    <header><div class="titulo-caixa">Titulo da caixa</div></header>
+                    <div class="texto-conteudo">
+                        Um conteúdo dentro da caixa de conteudo
+                    </div>
+                </div>
+                <div class="caixa-conteudo-texto caixa-conteudo">
+                    <header><div class="titulo-caixa">Titulo da caixa</div></header>
+                    <div class="texto-conteudo">
+                        Um conteúdo dentro da caixa de conteudo
+                    </div>
+                </div>
+            </section>
+            
+        </div>
 
     </div>
 </section>
